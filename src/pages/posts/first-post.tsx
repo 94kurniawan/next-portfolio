7535c07282bfa9f77668a450ce8420fa8829ed1a
BK <bk@nullreborn.com>
2023-03-20 21:53:12 +0700

    test env and axis

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -2,21 +2,22 @@ import Head from "next/head";
 // import Script from "next/script";
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function FirstPost() {
+    const requestAPI = async () => {
+        const response = await axios.get(`${process.env.BACKEND_API}/posts/1/comments`);
+        console.log('req API nih', response);
+    }
+    useEffect(() => {
+        requestAPI()
+    })
     return (
         <>
             <Head>
                 <title>First Post</title>
             </Head>
-            {/* <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() =>
-                console.log(`script loaded correctly, window.FB has been populated`)
-                }
-            /> */}
-
             {/* <img src="/images/profile.jpg" alt="My Name"/> */}
             <Image
                 src="/images/profile.jpg"
@@ -24,8 +25,9 @@ export default function FirstPost() {
                 width={144}
                 alt="My Profile Picture"
             />
-            <h1>First Post</h1>
+            <h1>First Page</h1>
             <h2><Link href="/">Back to home</Link></h2>
+            <hr />
         </>
     )
 }
