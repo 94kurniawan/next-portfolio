8c2f29452190817ad6c33b4c9fc32c6659d887ec
BK <bk@nullreborn.com>
2023-03-20 21:09:55 +0700

    command Script component

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Script from "next/script";
+// import Script from "next/script";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -9,13 +9,13 @@ export default function FirstPost() {
             <Head>
                 <title>First Post</title>
             </Head>
-            <Script
+            {/* <Script
                 src="https://connect.facebook.net/en_US/sdk.js"
                 strategy="lazyOnload"
                 onLoad={() =>
                 console.log(`script loaded correctly, window.FB has been populated`)
                 }
-            />
+            /> */}
 
             {/* <img src="/images/profile.jpg" alt="My Name"/> */}
             <Image
